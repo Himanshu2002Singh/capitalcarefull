5dd0a87947875c02f74a22c46b901aef3b5a89af
Priyanshu Mathur <mathur612priyanshu@gmailcom>
2025-05-28 19:05:29 +0530

    added loanType and status filters on leads page

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2025 at 02:22 PM
+-- Generation Time: May 28, 2025 at 03:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `priyanshu`
+-- Database: `capitalcaredb`
 --
 
 -- --------------------------------------------------------
@@ -28,20 +28,52 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `employees` (
-  `id` int(11) NOT NULL,
-  `emp_id` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `username` varchar(100) DEFAULT NULL,
-  `ename` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL
+  `emp_id` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `ename` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id`, `emp_id`, `email`, `username`, `ename`, `password`) VALUES
-(1, NULL, 'chandu@gmail.com', 'cc101chandu', 'Chandu', '1234567890');
+INSERT INTO `employees` (`emp_id`, `email`, `phone`, `username`, `ename`, `password`) VALUES
+('CCARE201', 'rajputhimanshusingh2002@gmail.com', '9161735718', 'CCARE201', 'Himanshu', '17605293'),
+('CCARE202', 'mathur612priyanshu@gmail.com', '9411619711', 'CCARE202', 'Priyanshu Mathur', '10307596');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `histories`
+--
+
+CREATE TABLE `histories` (
+  `history_id` int(11) NOT NULL,
+  `lead_id` int(11) NOT NULL,
+  `owner` varchar(255) DEFAULT NULL,
+  `next_meeting` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `histories`
+--
+
+INSERT INTO `histories` (`history_id`, `lead_id`, `owner`, `next_meeting`, `status`, `remark`, `createdAt`, `updatedAt`) VALUES
+(1, 2, 'John Doe', NULL, 'Follow-up', 'Customer interested in home loan, follow-up scheduled.', '2025-05-27 19:08:18', '2025-05-27 19:08:18'),
+(2, 1, 'Priyanshu Mathur', '2025-05-31 05:30:00', 'No Requirement', 'rem', '2025-05-27 19:11:32', '2025-05-27 19:11:32'),
+(4, 1, 'Priyanshu Mathur', '2025-05-30 05:30:00', 'Interested', '7j7jn7', '2025-05-28 12:09:51', '2025-05-28 12:09:51'),
+(5, 6, 'Priyanshu Mathur', '2025-05-28 05:30:00', 'No Requirement', ' hu. ', '2025-05-28 13:31:29', '2025-05-28 13:31:29'),
+(6, 1, 'Priyanshu Mathur', '2025-05-31 05:30:00', 'Interested', '', '2025-05-28 13:32:21', '2025-05-28 13:32:21'),
+(7, 7, 'Priyanshu Mathur', '2025-05-28 05:30:00', 'Documents Summited', NULL, '2025-05-28 13:37:12', '2025-05-28 13:37:12'),
+(8, 8, 'Priyanshu Mathur', '2025-05-30 05:30:00', 'Documents Summited', NULL, '2025-05-28 13:45:34', '2025-05-28 13:45:34'),
+(9, 11, 'Priyanshu Mathur', '2025-05-23 05:30:00', 'Documents Summited', NULL, '2025-05-28 13:48:40', '2025-05-28 13:48:40');
 
 -- --------------------------------------------------------
 
@@ -50,35 +82,42 @@ INSERT INTO `employees` (`id`, `emp_id`, `email`, `username`, `ename`, `password
 --
 
 CREATE TABLE `leads` (
-  `id` int(11) NOT NULL,
-  `person_id` int(11) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `number` varchar(20) DEFAULT NULL,
-  `owner` varchar(100) DEFAULT NULL,
-  `branch` varchar(20) DEFAULT NULL,
-  `source` varchar(20) DEFAULT NULL,
-  `priority` varchar(100) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL,
-  `next_meeting` varchar(100) DEFAULT NULL,
-  `refrence` varchar(100) DEFAULT NULL,
-  `description` varchar(200) DEFAULT NULL,
-  `est_budget` int(11) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
+  `lead_id` int(11) NOT NULL,
+  `person_id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `email` text DEFAULT NULL,
+  `owner` varchar(255) DEFAULT NULL,
+  `branch` varchar(255) DEFAULT NULL,
+  `source` varchar(255) DEFAULT NULL,
+  `priority` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `next_meeting` datetime DEFAULT NULL,
+  `refrence` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `loan_type` text DEFAULT NULL,
+  `est_budget` varchar(255) DEFAULT NULL,
+  `remark` text DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `leads`
 --
 
-INSERT INTO `leads` (`id`, `person_id`, `name`, `number`, `owner`, `branch`, `source`, `priority`, `status`, `next_meeting`, `refrence`, `description`, `est_budget`, `remark`, `createdAt`) VALUES
-(1, 1, 'Ravi Kumar', '9876543210', 'Chandu', 'Delhi', 'Website', 'Important', 'No Requirement', '2025-05-17', 'Google Ads', 'Looking for home loan', 8388, 'ffyc', '2025-05-15 10:21:56'),
-(2, 1, 'priyanshu', '12349846', 'priyanshu1', '', '', '', 'No Requirement', '2025-05-15', 'hd7eh', 'heebj', NULL, NULL, '2025-05-15 12:32:49'),
-(3, 1, 'priyanshu', '12349846', 'priyanshu1', '', '', '', 'No Requirement', '2025-05-15', 'hd7eh', 'heebj', NULL, NULL, '2025-05-15 12:34:27'),
-(4, 1, 'heyve', '9434', 'bshsv', 'Default', 'Newspaper', 'High Priority and Important', 'Interested', '2025-05-15', 'gsusv', 'hs8sb', NULL, NULL, '2025-05-15 12:36:13'),
-(6, 1, 'a', '11255', 'me', 'Default', 'Newspaper', 'Mid', 'Call Back', '', 'srf', 'fyv', NULL, NULL, '2025-05-16 07:01:09'),
-(7, 1, 'b', '427566', 'me', 'Default', 'Newspaper', 'Mid', 'No Requirement', '', 'vsub', 'beurb', NULL, NULL, '2025-05-16 11:33:21'),
-(8, 1, 'c', '86858', 'Preassigned owner Name', 'Default', 'Newspaper', 'Important', 'Document Rejected', '', 'hhib', 'hjb', NULL, NULL, '2025-05-16 11:41:28');
+INSERT INTO `leads` (`lead_id`, `person_id`, `name`, `number`, `email`, `owner`, `branch`, `source`, `priority`, `status`, `next_meeting`, `refrence`, `description`, `address`, `loan_type`, `est_budget`, `remark`, `createdAt`, `updatedAt`) VALUES
+(1, 'CCARE202', 'Eve Blue', '123', 'mathur@gmail.com', 'Priyanshu Mathur', '', 'LinkedIn', 'Important', 'Interested', '2025-05-31 05:30:00', 'Old Client', 'Looking for business expansion loan', 'Agra loha mandi', 'Business Loan', '', '', '2025-05-26 16:26:35', '2025-05-28 13:32:21'),
+(2, 'CCARE202', 'ceuev', '7659', 'gshycg', 'Priyanshu Mathur', '', 'Website', 'High Priority and Important', 'Follow up', '2025-05-26 05:30:00', 'bridge di', 'nsbdud d', 'bz8svsudve', 'Mortgage Loan', NULL, NULL, '2025-05-26 16:38:52', '2025-05-26 16:38:52'),
+(4, 'CCARE202', 'Eve Blue', '9876543266', 'mathur@gmail.com', 'Sunil', 'Hyderabad', 'LinkedIn', 'Low', 'Contacted', '2025-06-05 10:00:00', 'Old Client', 'Looking for business expansion loan', 'Agra loha mandi', 'Business Loan', NULL, NULL, '2025-05-27 11:39:08', '2025-05-27 11:39:08'),
+(5, 'CCARE202', 'yvvvyvv6v', '882888', 't. tv', 'Priyanshu Mathur', '', 'Newspaper', 'Important', 'Loan Section', '2025-05-29 05:30:00', ' g. y\n', 'g g. ', 'g. g vy bbybvtvtvyvm', 'User Car Loan', NULL, NULL, '2025-05-27 12:20:09', '2025-05-27 12:20:09'),
+(6, 'CCARE202', 'uehwu', '3446', 'vsusv', 'Priyanshu Mathur', 'Default', 'Website', 'Lower', 'No Requirement', '2025-05-28 05:30:00', 'bsbs', '', '', '', '9969', ' hu. ', '2025-05-27 15:00:15', '2025-05-28 13:31:29'),
+(7, 'CCARE202', 'vsjsv', '97648', 'vsjsv', 'Priyanshu Mathur', 'Default', 'Newspaper', 'Important', 'Document Rejected', '2025-05-22 05:30:00', 'vsjd ', 'vudv', 'bsidv', 'Mortgage Loan', NULL, NULL, '2025-05-28 13:29:39', '2025-05-28 13:29:39'),
+(8, 'CCARE202', 'beuev', '65348', 'hsuvd', 'Priyanshu Mathur', 'Default', 'Website', 'Mid', 'Documents Summited', '2025-05-28 05:30:00', 'h', 'jjsj', 'bsubd', 'User Car Loan', NULL, NULL, '2025-05-28 13:37:12', '2025-05-28 13:37:12'),
+(9, 'CCARE202', 'heye', '4649', 'shsv', 'Priyanshu Mathur', 'Default', 'Refrence', 'High Priority and Important', 'Documents Summited', '2025-05-30 05:30:00', 'bsbs', 'hsusv', 'hsdb', 'Personal Loan', NULL, NULL, '2025-05-28 13:45:34', '2025-05-28 13:45:34'),
+(10, 'CCARE202', 'heusb', '97979', 'hshdh', 'Priyanshu Mathur', '', 'Newspaper', 'Important', 'Documents Summited', '2025-05-28 05:30:00', 'bub', 'bub', 'bhv', '', NULL, NULL, '2025-05-28 13:47:09', '2025-05-28 13:47:09'),
+(11, 'CCARE202', 'ge7ge', '98359', '7dveusv', 'Priyanshu Mathur', '', 'Website', 'High Priority and Important', 'Documents Summited', '2025-05-23 05:30:00', 'vuvh', 'bhvh', 'vhbh', '', NULL, NULL, '2025-05-28 13:48:40', '2025-05-28 13:48:40');
 
 --
 -- Indexes for dumped tables
@@ -88,40 +127,37 @@ INSERT INTO `leads` (`id`, `person_id`, `name`, `number`, `owner`, `branch`, `so
 -- Indexes for table `employees`
 --
 ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`emp_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `histories`
+--
+ALTER TABLE `histories`
+  ADD PRIMARY KEY (`history_id`);
 
 --
 -- Indexes for table `leads`
 --
 ALTER TABLE `leads`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `person_id` (`person_id`);
+  ADD PRIMARY KEY (`lead_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `employees`
+-- AUTO_INCREMENT for table `histories`
 --
-ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `histories`
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `leads`
 --
 ALTER TABLE `leads`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `leads`
---
-ALTER TABLE `leads`
-  ADD CONSTRAINT `leads_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE;
+  MODIFY `lead_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- frontend/lib/views/screens/leads/leads_screen.dart
+++ frontend/lib/views/screens/leads/leads_screen.dart
@@ -1,5 +1,6 @@
 import 'package:capital_care/controllers/providers/lead_provider.dart';
 import 'package:capital_care/controllers/providers/userprovider.dart';
+import 'package:capital_care/theme/appcolors.dart';
 import 'package:capital_care/views/screens/leads/add_lead_screen.dart';
 import 'package:capital_care/views/screens/leads/lead_details_screen.dart';
 import 'package:capital_care/views/widgets/app_scaffold.dart';
@@ -16,24 +17,85 @@ class LeadsScreen extends StatefulWidget {
 }
 
 class _LeadsScreenState extends State<LeadsScreen> {
+  String loanSelectedItem = "All";
+
+  List<String> loanTypeOptions = [
+    "All",
+    "Home Loan",
+    "Mortgage Loan",
+    "User Car Loan",
+    "Business Loan",
+    "Personal Loan",
+    "Other",
+  ];
+  String selectedStatusItem = "All";
+  List<String> statusOptions = [
+    "All",
+    "Interested",
+    "Call Back",
+    "No Requirement",
+    "Follow up",
+    "Document Rejected",
+    "Documents Summited",
+    "Loan Section",
+    "Loan Disbursement",
+  ];
+
   @override
   void initState() {
     super.initState();
-    // Fetch leads only once when screen initializes
     Provider.of<LeadProvider>(context, listen: false).fetchLeads();
   }
 
   @override
   Widget build(BuildContext context) {
-    final leadProvider = Provider.of<LeadProvider>(context, listen: true);
+    final leadProvider = Provider.of<LeadProvider>(context);
     final leads = leadProvider.leads;
     final isLoading = leadProvider.isLoading;
-
     final user = Provider.of<UserProvider>(context).user;
+
+    /// 🔥 Filter logic
+    final filteredLeads =
+        loanSelectedItem == "All"
+            ? leads
+            : leads.where((lead) => lead.loanType == loanSelectedItem).toList();
+
+    final statusFilteredLeads =
+        selectedStatusItem == "All"
+            ? filteredLeads
+            : filteredLeads
+                .where((lead) => lead.status == selectedStatusItem)
+                .toList();
 
     return AppScaffold(
       isFloatingActionButton: true,
-      appBar: CustomAppbar(title: "Leads"),
+      appBar: CustomAppbar(
+        title: "Leads",
+        action: [
+          const Text("Loan Type: ", style: TextStyle(color: Colors.white)),
+          DropdownButton<String>(
+            value: loanSelectedItem,
+            style: const TextStyle(color: Colors.white),
+            dropdownColor: Colors.black,
+            iconEnabledColor: Colors.white,
+            items:
+                loanTypeOptions.map((type) {
+                  return DropdownMenuItem(
+                    value: type,
+                    child: Text(
+                      type,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  );
+                }).toList(),
+            onChanged: (value) {
+              setState(() {
+                loanSelectedItem = value!;
+              });
+            },
+          ),
+        ],
+      ),
       body:
           isLoading
               ? const Center(child: CircularProgressIndicator())
@@ -45,14 +107,64 @@ class _LeadsScreenState extends State<LeadsScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("Total Leads: ${leads.length}"),
+                      padding: const EdgeInsets.only(top: 8, left: 8),
+                      child: Text("Total Leads: ${statusFilteredLeads.length}"),
                     ),
+                    //  Status Filter
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: statusOptions.length,
+                        itemBuilder: (context, index) {
+                          final option = statusOptions[index];
+                          return GestureDetector(
+                            onTap: () {
+                              selectedStatusItem = option;
+                              setState(() {});
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 4,
+                                vertical: 8,
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 5,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: AppColors.primaryColor,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                                color:
+                                    selectedStatusItem == option
+                                        ? const Color.fromARGB(
+                                          255,
+                                          219,
+                                          219,
+                                          219,
+                                        )
+                                        : Colors.white,
+                              ),
+                              child: Text(
+                                option,
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+
+                    // Leads List
                     Expanded(
                       child: ListView.builder(
-                        itemCount: leads.length,
+                        itemCount: statusFilteredLeads.length,
                         itemBuilder: (context, index) {
-                          final lead = leads[index];
+                          final lead = statusFilteredLeads[index];
                           return GestureDetector(
                             onTap: () {
                               Navigator.push(
