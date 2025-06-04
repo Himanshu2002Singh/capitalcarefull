8fb0d690098034ffa4d4d2fd94f21f9e41cec076
Priyanshu Mathur <mathur612priyanshu@gmailcom>
2025-06-04 16:57:14 +0530

    server url corrected

--- frontend/lib/constants/server_url.dart
+++ frontend/lib/constants/server_url.dart
@@ -1,2 +1,2 @@
-const ServerUrl = "http://192.168.29.215:5000/api";
-// const ServerUrl = "http://16.171.57.253:5000/api";
+// const ServerUrl = "http://192.168.29.215:5000/api";
+const ServerUrl = "http://16.171.57.253:5000/api";
