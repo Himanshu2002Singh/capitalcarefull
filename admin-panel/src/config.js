57b9a5c18d3542fc6e286631f1327c044ad1ab5b
Priyanshu Mathur <mathur612priyanshu@gmailcom>
2025-06-17 13:36:33 +0530

    ip change

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -1,4 +1,4 @@
-let API_URL = "http://16.171.57.253:5000/api";
+let API_URL = "http://51.20.76.55 :5000/api";
 // let API_URL = "http://localhost:5000/api"
 // export default API_URL;
 export default API_URL;
